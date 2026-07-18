bccad9c9a28d3c2596040ab9425bfd316d11a63d
Arne <148494543+kulligo@users.noreply.github.com>
2026-07-18 18:34:36 +0200

    update

---
+++ js/app.js
@@ -0,0 +1,35 @@
+const content = document.getElementById("mainContent");
+
+const pages = {
+  pomodore: "pages/pomodore.html",
+  start: "pages/start.html",
+  todo: "pages/todo.html", 
+}
+
+async function loadPage(pageName) {
+  const pagePath = pages[pageName];
+
+  if (!pagePath) {
+    content.innerHTML = "<p>Seite nicht gefunden.</p>"
+    return;
+  }
+
+  try {
+    const response = await fetch(pagePath);
+
+    if (!response.ok) {
+      throw new Error("Seite konnte nicht geladen werden");
+    }
+
+    content.innerHTML = await response.text();
+  } catch (error) {
+    content.innerHTML = "<p>Der Inhalt konte nicht geladen werden.</p>";
+    console.error(error);
+  }
+}
+
+document.querySelectorALL("[data-page").forEach(("button") => {
+    button.addEventListener("click", () => {
+      loadPage(button.dataset.page);
+  });
+)}:
